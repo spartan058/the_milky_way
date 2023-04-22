9599986cdffc13a13b9063427149b69a38b76829
spartan058 <hyxk1120302996@163.co>
2023-04-22 23:36:48 +0800

    message

--- main.py
+++ main.py
@@ -76,21 +76,22 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 
 for i in range(len(user_ids)):
-    wea, tem = get_weather(citys[i])
-    cit, dat = get_city_date(citys[i])
-    data = {
-        "date": {"value": "今日日期：{}".format(dat), "color": get_random_color()},
-        "city": {"value": "当前城市：{}".format(cit), "color": get_random_color()},
-        "weather": {"value": "今日天气：{}".format(wea), "color": get_random_color()},
-        "temperature": {"value": "当前温度：{}".format(tem), "color": get_random_color()},
-        "love_days": {"value": "今天是你们在一起的第{}天".format(get_count(start_dates[i])), "color": get_random_color()},
-        "birthday_left": {"value": "距离她的生日还有{}天".format(get_birthday(birthdays[i])), "color": get_random_color()},
-        "solary": {"value": "距离发工资还有{}天".format(get_solary(solarys[i])), "color": get_random_color()},
-        "words": {"value": get_words(), "color": get_random_color()}
-    }
-    if get_birthday(birthdays[i]) == 0:
-        data["birthday_left"]['value'] = "今天是她的生日哦，快去一起甜蜜吧"
-    if get_solary(solarys[i]) == 0:
-        data["solary"]['value'] = "今天发工资啦，快去犒劳一下自己吧"
+    # wea, tem = get_weather(citys[i])
+    # cit, dat = get_city_date(citys[i])
+    # data = {
+    #     "date": {"value": "今日日期：{}".format(dat), "color": get_random_color()},
+    #     "city": {"value": "当前城市：{}".format(cit), "color": get_random_color()},
+    #     "weather": {"value": "今日天气：{}".format(wea), "color": get_random_color()},
+    #     "temperature": {"value": "当前温度：{}".format(tem), "color": get_random_color()},
+    #     "love_days": {"value": "今天是你们在一起的第{}天".format(get_count(start_dates[i])), "color": get_random_color()},
+    #     "birthday_left": {"value": "距离她的生日还有{}天".format(get_birthday(birthdays[i])), "color": get_random_color()},
+    #     "solary": {"value": "距离发工资还有{}天".format(get_solary(solarys[i])), "color": get_random_color()},
+    #     "words": {"value": get_words(), "color": get_random_color()}
+    # }
+    # if get_birthday(birthdays[i]) == 0:
+    #     data["birthday_left"]['value'] = "今天是她的生日哦，快去一起甜蜜吧"
+    # if get_solary(solarys[i]) == 0:
+    #     data["solary"]['value'] = "今天发工资啦，快去犒劳一下自己吧"
+    data = {}
     res = wm.send_template(user_ids[i], template_ids[i], data)
     print(res)
