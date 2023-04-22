01a6fcb1529dd2a43d7a9a7f111e46f5b7fdb0ff
spartan058 <hyxk1120302996@163.co>
2023-04-23 00:19:48 +0800

    message

--- main.py
+++ main.py
@@ -24,54 +24,54 @@ birthdays = os.environ["BIRTHDAY"].split(',')
 cookies = os.environ["COOKIE"].split(',')
 
 
-# 获取天气和温度
-def get_weather(city):
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-    res = requests.get(url).json()
-    weather = res['data']['list'][0]
-    return weather['weather'], math.floor(weather['temp'])
+# # 获取天气和温度
+# def get_weather(city):
+#     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+#     res = requests.get(url).json()
+#     weather = res['data']['list'][0]
+#     return weather['weather'], math.floor(weather['temp'])
 
 
-# 当前城市、日期
-def get_city_date(city):
-    return city, today.date().strftime("%Y-%m-%d")
+# # 当前城市、日期
+# def get_city_date(city):
+#     return city, today.date().strftime("%Y-%m-%d")
 
 
-# 距离设置的日期过了多少天
-def get_count(start_date):
-    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-    return delta.days
+# # 距离设置的日期过了多少天
+# def get_count(start_date):
+#     delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+#     return delta.days
 
 
-# 距离发工资还有多少天
-def get_solary(solary):
-    next = datetime.strptime(str(date.today().year) + "-" + str(date.today().month) + "-" + solary, "%Y-%m-%d")
-    if next < datetime.now():
-        if next.month == 12:
-            next = next.replace(year=next.year + 1)
-        next = next.replace(month=(next.month + 1) % 12)
-    return (next - today).days
+# # 距离发工资还有多少天
+# def get_solary(solary):
+#     next = datetime.strptime(str(date.today().year) + "-" + str(date.today().month) + "-" + solary, "%Y-%m-%d")
+#     if next < datetime.now():
+#         if next.month == 12:
+#             next = next.replace(year=next.year + 1)
+#         next = next.replace(month=(next.month + 1) % 12)
+#     return (next - today).days
 
 
-# 距离过生日还有多少天
-def get_birthday(birthday):
-    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-    if next < datetime.now():
-        next = next.replace(year=next.year + 1)
-    return (next - today).days
+# # 距离过生日还有多少天
+# def get_birthday(birthday):
+#     next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+#     if next < datetime.now():
+#         next = next.replace(year=next.year + 1)
+#     return (next - today).days
 
 
-# 每日一句
-def get_words():
-    words = requests.get("https://api.shadiao.pro/chp")
-    if words.status_code != 200:
-        return get_words()
-    return words.json()['data']['text']
+# # 每日一句
+# def get_words():
+#     words = requests.get("https://api.shadiao.pro/chp")
+#     if words.status_code != 200:
+#         return get_words()
+#     return words.json()['data']['text']
 
 
-# 字体随机颜色
-def get_random_color():
-    return "#%06x" % random.randint(0, 0xFFFFFF)
+# # 字体随机颜色
+# def get_random_color():
+#     return "#%06x" % random.randint(0, 0xFFFFFF)
 
 # 签到
 def check_in():
@@ -92,7 +92,7 @@ def get_leftdays():
         "cookie": cookies[0]
     }
     res = requests.get(url,headers = headers).json()
-    leftDays = res.data.leftDays
+    leftDays = res['data']['leftDays']
     return leftDays
 
 
